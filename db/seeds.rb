d4967527a5808d2196834e1d8c56683f95251297
seyoungjoo <sellyjphoto@gmail.com>
2020-11-18 16:37:34 +0900

    restaurant rounte with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  attributes = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    }
+  restaurant = Restaurant.create!(attributes)
+
+  review_attributes = {
+    content: Faker::Name.name,
+    rating: rand(1..5), 
+    restaurant: restaurant
+  }
+  Review.create!(review_attributes)
+end
